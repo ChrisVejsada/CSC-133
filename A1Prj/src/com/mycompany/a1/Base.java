05f0b1f225c655e29979265b888bf69b87b03891
ChrisVejsada <chrisjvejsada@gmail.com>
2019-02-27 15:54:14 -0800

    First Commit

---
+++ A1Prj/src/com/mycompany/a1/Base.java
@@ -0,0 +1,35 @@
+package com.mycompany.a1;
+
+import com.codename1.charts.util.ColorUtil;
+
+public class Base extends Fixed{
+	
+
+	private int sequenceNumber;
+	//Sets the base color
+	public Base(int size, int sequenceNumber, double x, double y) {
+		super(size, x, y);
+		super.setColor(ColorUtil.rgb(0, 255, 0));
+		this.sequenceNumber = sequenceNumber;
+	}
+	//Getter fpr number of base
+	public int getSequenceNumber() {
+		return sequenceNumber;
+	}
+	
+	@Override
+	public void setColor(int color) {
+		
+	}
+	
+	@Override
+	public String toString() {
+		String parentDesc = super.toString();
+		String myDesc = " sequence Number="+sequenceNumber;
+		return "Base:" + parentDesc + myDesc;
+	}
+	
+}
+	
+	
+	
