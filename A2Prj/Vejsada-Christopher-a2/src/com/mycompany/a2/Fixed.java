e72756d903d27619ca2c81a75f88b9ad09643a44
ChrisVejsada <chrisjvejsada@gmail.com>
2019-03-18 04:45:18 -0700

    Final Turn in build compressed

---
+++ A2Prj/Vejsada-Christopher-a2/src/com/mycompany/a2/Fixed.java
@@ -0,0 +1,29 @@
+package com.mycompany.a2;
+
+import com.mycompany.a2.GameObject;
+
+public abstract class Fixed extends GameObject{
+	
+	/**
+	 * Constuctor for fixed
+	 * @param x
+	 * @param y
+	 */
+	
+	public Fixed(int size, double x, double y) {
+		super(size, x, y);
+	}
+	
+	/**
+	 * Override that does not allow fixed to set x and y
+	 */
+	@Override
+	public void setX(double x) {
+		
+	}
+	@Override
+	public void setY(double y) {
+		
+	}
+
+}
