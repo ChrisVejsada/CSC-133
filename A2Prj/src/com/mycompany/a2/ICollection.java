3c7c636ab1029520b440b9936764e8268229db13
ChrisVejsada <chrisjvejsada@gmail.com>
2019-04-15 01:54:19 -0700

    Got build for A2 working again

---
+++ A2Prj/src/com/mycompany/a2/ICollection.java
@@ -0,0 +1,10 @@
+package com.mycompany.a2;
+
+public interface ICollection {
+	
+	public void add(Object object);
+	
+	public IIterator getIterator();
+	
+
+}
---
+++ A2Prj/src/com/mycompany/a2/ISteerable.java
@@ -0,0 +1,11 @@
+package com.mycompany.a2;
+
+public interface ISteerable {
+	
+	/**
+	 * Allows the object to be steered
+	 * @param changeHeading
+	 */
+	void changeHeading(char s);
+
+}
